a3220755fb8c5c218529f4a66d290b0f73fd0cc6
Priyanshu <priyanshu.kr81@gmail.com>
2023-06-27 10:32:40 +0530

    added elevator-status of any particular building

--- elevator/api_views.py
+++ elevator/api_views.py
@@ -1,9 +1,19 @@
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, ListAPIView
 
-from .models import BuildingConfiguration
-from .serializers import BuildingConfigurationSerializer
+from .models import BuildingConfiguration, Elevator
+from .serializers import BuildingConfigurationSerializer, ElevatorSerializer
 
 
 class BuildingConfigurationCreateAPIView(ListCreateAPIView):
     queryset = BuildingConfiguration.objects.all()
     serializer_class = BuildingConfigurationSerializer
+
+
+class ElevatorListAPIView(ListAPIView):
+    queryset = Elevator.objects.all()
+    serializer_class = ElevatorSerializer
+
+    def get_queryset(self):
+        building_uuid = self.kwargs['building']
+        queryset = Elevator.objects.filter(building__id=building_uuid)
+        return queryset
--- elevator/urls.py
+++ elevator/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 
-from .api_views import BuildingConfigurationCreateAPIView
+from .api_views import BuildingConfigurationCreateAPIView, ElevatorListAPIView
 
 urlpatterns = [
-    path('building-configuration/', BuildingConfigurationCreateAPIView.as_view(), name='building-configuration-create')
+    path('building-configuration/', BuildingConfigurationCreateAPIView.as_view(), name='building-configuration-create'),
+    path('elevators-status/<uuid:building>', ElevatorListAPIView.as_view(), name='elevator-status-list-view')
 ]
